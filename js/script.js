0112ee1b774170d5b68a6587995add1f0bd27aaa
Prabin Magar <psmagar123451@gmail.com>
2022-08-16 14:54:34 +0545

    links and hover effects

--- js/script.js
+++ js/script.js
@@ -16,9 +16,23 @@ $(document).ready(function(){
     });
 
     // side navigation toggle
+    const navbarOverlayModal = $('.navbar-overlay-modal');
     const sideNavbarDiv = $('.sc-side-navbar');
-    $('#show-sidenav-btn').click(() => sideNavbarDiv.addClass('js-show-navbar'));
-    $('#hide-sidenav-btn').click(() => sideNavbarDiv.removeClass('js-show-navbar'));
+    $('#show-sidenav-btn').click(() => {
+        sideNavbarDiv.addClass('js-show-navbar');
+        navbarOverlayModal.addClass('js-show-navbar-modal');
+    });
+    $('#hide-sidenav-btn').click(() => {
+        sideNavbarDiv.removeClass('js-show-navbar');
+        navbarOverlayModal.removeClass('js-show-navbar-modal');
+    });
+
+    $(document.body).click(function(e){
+        if($(e.target).hasClass('js-show-navbar-modal') || $(e.target).hasClass('side-nav-link')){
+            sideNavbarDiv.removeClass('js-show-navbar');
+            navbarOverlayModal.removeClass('js-show-navbar-modal');
+        }
+    })
 
     sliderIdx3.owlCarousel({
         center: true,
@@ -46,7 +60,7 @@ $(document).ready(function(){
         loop: true,
         nav: true,
         margin: 25,
-        stagePadding: 10,
+        stagePadding: 15,
         navText: ["<i class='fa fa-chevron-left fa-xl'></i>","<i class='fa fa-chevron-right fa-xl'></i>"]
     })
 
@@ -79,7 +93,7 @@ $(document).ready(function(){
         items: 2,
         dots: true,
         nav: true,
-        loop: true,
+        loop: false,
         margin: 10,
         slideBy: 1,
         navText: ["<i class='fa fa-chevron-left fa-xl'></i>","<i class='fa fa-chevron-right fa-xl'></i>"],
@@ -98,7 +112,7 @@ $(document).ready(function(){
     const itemList = $('#gallery-itm-list .itm-card');
     itemList.each(function(index, element){
         $(element).click(function(){
-            let tempContent = $(element).clone();
+            let tempContent = $(element).children().clone();
             itemDisplayContainer.html(tempContent);
             let url = $(element).children('.itm-card-body').attr('data-url');
             let imgpath = $(element).children('.itm-card-body').attr('data-img');
@@ -107,8 +121,18 @@ $(document).ready(function(){
     })
 
     const setContent = (url, imgpath) => {
-        itemDisplayContainer.children("#display-img").attr('src', imgpath);
-        itemDisplayContainer.children("#display-url").attr('href', url);
-        itemDisplayContainer.children("#display-title").addClass('gallery-item-title');
+        itemDisplayContainer.children(".display-img").attr('src', imgpath);
+        itemDisplayContainer.attr('href', url);
+        itemDisplayContainer.children().children(".display-title").addClass('gallery-item-title');
+        itemDisplayContainer.children().children('.display-info').removeClass('d-none')
     }
+
+    const intitalContent = () => {
+        let tempContent = $(itemList[0]).children().clone();
+        itemDisplayContainer.html(tempContent);
+        let url = $(itemList[0]).children('.itm-card-body').attr('data-url');
+        let imgpath = $(itemList[0]).children('.itm-card-body').attr('data-img');
+        setContent(url, imgpath);
+    }
+    intitalContent();
 })
